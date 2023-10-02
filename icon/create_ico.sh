5ef0b60648276b8514b6812674ef3acea77c8b82
wilderMann <andreas.wildmann@gmail.com>
2023-10-02 17:15:37 +0200

    setup scripts for win, linux and macos

---
+++ icon/create_ico.sh
@@ -0,0 +1,2 @@
+ 
+convert icon16.png icon24.png icon32.png icon48.png icon256.png icon.ico
--- setup_win.bat
+++ setup_win.bat
@@ -1,5 +1,5 @@
 @echo off
 
-pyinstaller --onefile --name "FlatCAM Carvera Postprozessor" --windowed main.py
+pyinstaller --onefile --name "FlatCAM Carvera Postprozessor" --windowed --icon=./icon/icon.ico main.py
 
 pause
